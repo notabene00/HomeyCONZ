001fc155163ab22a8b0d25f65ca667edd4f99db0
Björn Kallin <48549512+k4mrat@users.noreply.github.com>
2019-11-29 15:59:54 +0100

    Add files via upload

---
+++ tradfri-square-dimmer/driver.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Driver = require('../Driver')
+
+class TradfriSquareDimmerDriver extends Driver {
+	
+	onInit() {
+		super.onInit()
+		this.log('TradfriRemoteControlDriver has been inited')
+	}
+	
+	onPairListDevices(data, callback) {
+		this.getSensorsByCondition(device => device.modelid === 'TRADFRI on/off switch', callback)
+	}
+	
+}
+
+module.exports = TradfriSquareDimmerDriver
