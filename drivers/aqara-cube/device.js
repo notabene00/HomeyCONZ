542e829041b7d31f684c75670f96cb68798a2c92
fantomnotabene <fantomnotabene@gmail.com>
2020-01-16 21:57:05 +0300

    capability side up

--- drivers/aqara-cube/device.js
+++ drivers/aqara-cube/device.js
@@ -11,14 +11,13 @@ class Cube extends Sensor {
 		this.setTriggers()
 		this.setConditions()
 		
-		this.side = 0
-
 		this.log(this.getName(), 'has been initiated')
 	}
 	
 	fireEvent(number, initial=false, gesture=null) {
-		if (gesture !== 7 && gesture !== 8) {
-			this.side = parseInt(number.toString().charAt(0)) // first digit
+		if (gesture == 3 || gesture == 4) {
+			let side = parseInt(number.toString().charAt(0)) // first digit
+			this.setCapabilityValue('side_up', side)
 			if (initial) return
 		}
 		if (gesture == 0) {
@@ -62,27 +61,27 @@ class Cube extends Sensor {
 	setConditions() {
 		this.isFirstSide = new Homey.FlowCardCondition('is_first_side').registerRunListener((args, state, callback) => {
 			console.log(args.device.side)
-			callback(null, args.device.side == 1)
+			callback(null, args.device.getCapabilityValue('side_up') == 1)
 		}).register()
 		this.isSecondSide = new Homey.FlowCardCondition('is_second_side').registerRunListener((args, state, callback) => {
 			console.log(args.device.side)
-			callback(null, args.device.side == 2)
+			callback(null, args.device.getCapabilityValue('side_up') == 2)
 		}).register()
 		this.isThirdSide = new Homey.FlowCardCondition('is_third_side').registerRunListener((args, state, callback) => {
 			console.log(args.device.side)
-			callback(null, args.device.side == 3)
+			callback(null, args.device.getCapabilityValue('side_up') == 3)
 		}).register()
 		this.isFourthSide = new Homey.FlowCardCondition('is_fourth_side').registerRunListener((args, state, callback) => {
 			console.log(args.device.side)
-			callback(null, args.device.side == 4)
+			callback(null, args.device.getCapabilityValue('side_up') == 4)
 		}).register()
 		this.isFifthSide = new Homey.FlowCardCondition('is_fifth_side').registerRunListener((args, state, callback) => {
 			console.log(args.device.side)
-			callback(null, args.device.side == 5)
+			callback(null, args.device.getCapabilityValue('side_up') == 5)
 		}).register()
 		this.isSixthSide = new Homey.FlowCardCondition('is_sixth_side').registerRunListener((args, state, callback) => {
 			console.log(args.device.side)
-			callback(null, args.device.side == 6)
+			callback(null, args.device.getCapabilityValue('side_up') == 6)
 		}).register()
 	}
 	
