9dd2549525797c5fe62415c91491b438c4c77c29
MadMonkey87 <myself@philippewechsler.ch>
2020-04-12 15:33:18 +0200

    raw event trigger for generic switches

--- drivers/generic-wireless-switch/device.js
+++ drivers/generic-wireless-switch/device.js
@@ -14,56 +14,62 @@ class GenericWirelessSwitch extends Sensor {
 	}
 	
 	fireEvent(number) {
-		switch (number) {
-			case 1002:
-				this.log('left clicked')
-				this.triggerLeftClicked.trigger(this)
-				break		
-			case 2002:
-				this.log('right clicked')
-				this.triggerRightClicked.trigger(this)
-				break
-			case 3002:
-				this.log('both clicked')
-				this.triggerBothClicked.trigger(this)
-				break
-			case 1001:
-				this.log('left held')
-				this.triggerLeftHeld.trigger(this)
-				break		
-			case 2001:
-				this.log('right held')
-				this.triggerRightHeld.trigger(this)
-				break
-			case 3001:
-				this.log('both held')
-				this.triggerBothHeld.trigger(this)
-				break
-			case 1004:
-				this.log('left double clicked')
-				this.triggerLeftDoubleClicked.trigger(this)
-				break		
-			case 2004:
-				this.log('right double clicked')
-				this.triggerRightDoubleClicked.trigger(this)
-				break
-			case 3004:
-				this.log('both double clicked')
-				this.triggerBothDoubleClicked.trigger(this)
-				break
+
+		let buttonIndex = (number - (number % 1000))/1000;
+		let actionIndex = number % 1000;
+		let action = 'unknown';
+		switch(actionIndex){
+			case 0: 
+				action = 'initial_press';
+				break;
+			case 1: 
+				action = 'hold';
+				break;
+			case 2: 
+				action = 'release_after_press';
+				break;
+			case 3: 
+				action = 'release_after_hold';
+				break;
+			case 4: 
+				action = 'double_press';
+				break;
+			case 5: 
+				action = 'tripple_press';
+				break;
+			case 6: 
+				action = 'quadruple_press';
+				break;
+			case 7: 
+				action = 'shake';
+				break;
+			case 8: 
+				action = 'drop';
+				break;
+			case 9: 
+				action = 'tilt';
+				break;
+			case 10: 
+				action = 'many_presses';
+				break;
 		}
+
+		this.log('button ' + buttonIndex + ', action '+ action);
+		this.triggerRaw.trigger(this, {rawEvent: number, button: buttonIndex, actionIndex: actionIndex, action: action});
 	}
 	
 	setTriggers() {
-		this.triggerLeftClicked = new Homey.FlowCardTriggerDevice('left_button_clicked').register()
-		this.triggerRightClicked = new Homey.FlowCardTriggerDevice('right_button_clicked').register()
-		this.triggerBothClicked = new Homey.FlowCardTriggerDevice('both_buttons_clicked').register()
-		this.triggerLeftHeld = new Homey.FlowCardTriggerDevice('left_button_held').register()
-		this.triggerRightHeld = new Homey.FlowCardTriggerDevice('right_button_held').register()
-		this.triggerBothHeld = new Homey.FlowCardTriggerDevice('both_buttons_held').register()
-		this.triggerLeftDoubleClicked = new Homey.FlowCardTriggerDevice('left_button_double_clicked').register()
-		this.triggerRightDoubleClicked = new Homey.FlowCardTriggerDevice('right_button_double_clicked').register()
-		this.triggerBothDoubleClicked = new Homey.FlowCardTriggerDevice('both_buttons_double_clicked').register()
+		this.triggerRaw = new Homey.FlowCardTriggerDevice('raw_switch_event').register();
+
+		this.triggerLeftClicked = new Homey.FlowCardTriggerDevice('left_button_clicked').register();
+		this.triggerRightClicked = new Homey.FlowCardTriggerDevice('right_button_clicked').register();
+		this.triggerBothClicked = new Homey.FlowCardTriggerDevice('both_buttons_clicked').register();
+		this.triggerLeftHeld = new Homey.FlowCardTriggerDevice('left_button_held').register();
+		this.triggerRightHeld = new Homey.FlowCardTriggerDevice('right_button_held').register();
+		this.triggerBothHeld = new Homey.FlowCardTriggerDevice('both_buttons_held').register();
+		this.triggerLeftDoubleClicked = new Homey.FlowCardTriggerDevice('left_button_double_clicked').register();
+		this.triggerRightDoubleClicked = new Homey.FlowCardTriggerDevice('right_button_double_clicked').register();
+		this.triggerBothDoubleClicked = new Homey.FlowCardTriggerDevice('both_buttons_double_clicked').register();
 	}
 	
 }
