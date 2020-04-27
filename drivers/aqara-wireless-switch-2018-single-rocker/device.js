d1af9795b86e4fa088df180d368290cfaa3bfbdf
MadMonkey87 <myself@philippewechsler.ch>
2020-04-27 18:07:06 +0200

    support for aqara wireless switch 2018 single rocker

---
+++ drivers/aqara-wireless-switch-2018-single-rocker/device.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Sensor = require('../Sensor')
+const Homey = require('homey')
+
+class AqaraWirelessSwitchSingleRocker2018 extends Sensor {
+	
+	onInit() {
+		super.onInit()
+		
+		this.setTriggers()
+		
+		this.log(this.getName(), 'has been initiated')
+	}
+	
+	fireEvent(number) {
+
+		const tokens = this.getSwitchEventTokens(number);
+		const state = {buttonIndex: tokens.buttonIndex.toString(), actionIndex: tokens.actionIndex.toString()};
+
+		this.log('generic switch event (' + number + ') button: ' + tokens.buttonIndex + ', action: '+ tokens.action);
+
+		this.triggerRaw.trigger(this, tokens, state);
+	}
+	
+	setTriggers() {
+		this.triggerRaw = new Homey.FlowCardTriggerDevice('raw_switch_event')
+		.register()
+		.registerRunListener((args, state) => {
+			return Promise.resolve(
+				(args.button === '-1' || args.button === state.buttonIndex) &&
+				(args.action === '-1' || args.action === state.actionIndex));
+		});
+	}
+	
+}
+
+module.exports = AqaraWirelessSwitchSingleRocker2018
---
+++ drivers/aqara-wireless-switch-2018-single-rocker/driver.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Driver = require('../Driver')
+
+class AqaraWirelessSwitch2018SingleRockerDriver extends Driver {
+	
+	onInit() {
+		super.onInit()
+		this.log('AqaraWirelessSwitch2018SingleRockerDriver has been initiated')
+	}
+	
+	onPairListDevices(data, callback) {
+		this.getSensorsByCondition(device => device.modelid === 'lumi.remote.b186acn01', callback)
+	}
+	
+}
+
+module.exports = AqaraWirelessSwitch2018SingleRockerDriver
