9d8c42e564212539b7756b7c8dbc84f321ce1d94
MadMonkey87 <myself@philippewechsler.ch>
2020-05-17 20:31:34 +0200

    update hardware settings (sensitivity, led indiction)

--- app.js
+++ app.js
@@ -376,6 +376,9 @@ class deCONZ extends Homey.App {
 		if (config.hasOwnProperty('sensitivity') && device.getSetting('sensitivity') != null) {
 			device.setSettings({ sensitivity: config.sensitivity });
 		}
+		if (config.hasOwnProperty('ledindication') && device.getSetting('ledindication') != null) {
+			device.setSettings({ ledindication: config.ledindication });
+		}
 		if (config.hasOwnProperty('reachable')) {
 			config.reachable ? device.setAvailable() : device.setUnavailable('Unreachable')
 		}
