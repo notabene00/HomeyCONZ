3823a198e623cd7a38aa2d6e41ea5258c15d404d
MadMonkey87 <myself@philippewechsler.ch>
2020-05-19 12:20:57 +0200

    secondary no motion trigger for all motion sensors

--- drivers/aqara-motion/device.js
+++ drivers/aqara-motion/device.js
@@ -28,19 +28,23 @@ class AqaraMotion extends Sensor {
 
 					this.secondaryTimeout = setTimeout(() => {
 						this.triggerSecondaryNoMotion.trigger(this)
+						this.secondaryTimeout = null
 					}, this.getSetting('secondary_no_motion_timeout') * 1000)
 
 				}, this.getSetting('no_motion_timeout') * 1000)
 			} else {
 				// motion detected
+
+				if(this.secondaryTimeout){
+					clearTimeout(this.secondaryTimeout)
+					this.secondaryTimeout = null
+				}
+
 				if (this.timeout) {
 					// if you have a timer, clear it
 					// if there is a timer, the sensor still detects movement
 					clearTimeout(this.timeout)
 					this.timeout = null
-
-					clearTimeout(this.secondaryTimeout)
-					this.secondaryTimeout = null
 				} else {
 					// if there is no timer, make the sensor in the kolobok detect movement
 					super.setCapabilityValue(name, true)
--- drivers/tradfri-motion-sensor/device.js
+++ drivers/tradfri-motion-sensor/device.js
@@ -30,36 +30,40 @@ class TradfriMotion extends Sensor {
 			super.setCapabilityValue(name, value)
 		} 
 		else if (name === 'alarm_motion') {
-				if (!value) {
-					// no motion detected
-					// set the timer to turn off the sensor
-					this.timeout = setTimeout(() => {
-						super.setCapabilityValue(name, false)
-						this.timeout = null
-	
-						this.secondaryTimeout = setTimeout(() => {
-							this.triggerSecondaryNoMotion.trigger(this)
-						}, this.getSetting('secondary_no_motion_timeout') * 1000)
-	
-					}, this.getSetting('no_motion_timeout') * 1000)
-				} else {
-					// motion detected
-					if (this.timeout) {
-						// if you have a timer, clear it
-						// if there is a timer, the sensor still detects movement
-						clearTimeout(this.timeout)
-						this.timeout = null
-	
-						clearTimeout(this.secondaryTimeout)
+			if (!value) {
+				// no motion detected
+				// set the timer to turn off the sensor
+				this.timeout = setTimeout(() => {
+					super.setCapabilityValue(name, false)
+					this.timeout = null
+
+					this.secondaryTimeout = setTimeout(() => {
+						this.triggerSecondaryNoMotion.trigger(this)
 						this.secondaryTimeout = null
-					} else {
-						// if there is no timer, make the sensor in the kolobok detect movement
-						super.setCapabilityValue(name, true)
-					}
-				}
+					}, this.getSetting('secondary_no_motion_timeout') * 1000)
+
+				}, this.getSetting('no_motion_timeout') * 1000)
 			} else {
-				super.setCapabilityValue(name, value)
+				// motion detected
+
+				if(this.secondaryTimeout){
+					clearTimeout(this.secondaryTimeout)
+					this.secondaryTimeout = null
+				}
+
+				if (this.timeout) {
+					// if you have a timer, clear it
+					// if there is a timer, the sensor still detects movement
+					clearTimeout(this.timeout)
+					this.timeout = null
+				} else {
+					// if there is no timer, make the sensor in the kolobok detect movement
+					super.setCapabilityValue(name, true)
+				}
 			}
+		} else {
+			super.setCapabilityValue(name, value)
+		}
 	}
 	
 }
