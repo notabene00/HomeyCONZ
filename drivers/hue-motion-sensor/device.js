219b99715e5de4e8d82ecb8681041fa73868af19
MadMonkey87 <myself@philippewechsler.ch>
2020-06-10 17:54:29 +0200

    improved motion sensor handling

--- drivers/hue-motion-sensor/device.js
+++ drivers/hue-motion-sensor/device.js
@@ -15,6 +15,7 @@ class HueMotion extends Sensor {
 	
 	setCapabilityValue(name, value) {
 		if (name === 'alarm_motion') {
+
 			if (!value) {
 				// no motion detected
 
@@ -25,30 +26,35 @@ class HueMotion extends Sensor {
 				
 				// set the timer to turn off the sensor
 				this.timeout = setTimeout(() => {
-					super.setCapabilityValue(name, false)
+					clearTimeout(this.timeout)
 					this.timeout = null
 
+					if(this.getCapabilityValue('alarm_motion') === false){
+						return
+					}
+
+					super.setCapabilityValue(name, false)
+
 					this.secondaryTimeout = setTimeout(() => {
-						this.triggerSecondaryNoMotion.trigger(this)
+						clearTimeout(this.secondaryTimeout)
 						this.secondaryTimeout = null
+
+						this.triggerSecondaryNoMotion.trigger(this)
 					}, this.getSetting('secondary_no_motion_timeout') * 1000)
 
 				}, this.getSetting('no_motion_timeout') * 1000)
-			} else {
-				// motion detected
 
+			} else if (value) { // motion detected
+				
 				if(this.secondaryTimeout){
 					clearTimeout(this.secondaryTimeout)
 					this.secondaryTimeout = null
 				}
 
 				if (this.timeout) {
-					// if you have a timer, clear it
-					// if there is a timer, the sensor still detects movement
 					clearTimeout(this.timeout)
 					this.timeout = null
 				} else {
-					// if there is no timer, make the sensor in the kolobok detect movement
 					super.setCapabilityValue(name, true)
 				}
 			}
