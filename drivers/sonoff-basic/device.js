14e9b10f3a59881d3397178ec6df7245893073f2
MadMonkey87 <myself@philippewechsler.ch>
2020-06-16 08:27:16 +0200

    basic sonoff

---
+++ drivers/sonoff-basic/device.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Light = require('../Light')
+const { FlowCardTriggerDevice } = require('homey')
+
+class GenericPlug extends Light {
+	
+	onInit() {
+		super.onInit()
+
+		this.setTriggers()
+		
+		this.log(this.getName(), 'has been initiated')
+	}
+
+	setTriggers() {
+		this.triggerSwitched = new FlowCardTriggerDevice('switched').register()
+	}
+
+	setCapabilityValue(name, value) {
+		if (name === 'onoff' && value != this.getCapabilityValue(name)) {
+			this.triggerSwitched.trigger(this)
+		}
+		super.setCapabilityValue(name, value)
+	}
+	
+}
+
+module.exports = GenericPlug
