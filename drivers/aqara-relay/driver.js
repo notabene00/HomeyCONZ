bac7b43695404fa4a6bef7970b23926a6b2f09db
MadMonkey87 <myself@philippewechsler.ch>
2020-06-18 17:14:44 +0200

    aqara 2-gang relay

--- drivers/aqara-relay/driver.js
+++ drivers/aqara-relay/driver.js
@@ -10,6 +10,9 @@ class AqaraRelayDriver extends Driver {
 	}
 	
 	onPairListDevices(data, callback) {
+		// todo: handle the second light by either
+		// -> provide two separate lights, then need to ensure that the measuremenet applies to both (or simply drop that)?
+		// -> provide one device with two light controlls
 		this.getLightsByCondition(device => device.modelid === 'lumi.relay.c2acn01', callback)
 	}
 }
