643e63b76efc95e4af9c5af08c70790a7031d523
MadMonkey87 <myself@philippewechsler.ch>
2020-07-23 17:51:52 +0200

    improved water leakage support

--- drivers/zipato-water-leakage/driver.js
+++ drivers/zipato-water-leakage/driver.js
@@ -10,7 +10,7 @@ class ZipatoWaterLeakageDriver extends Driver {
 	}
 	
 	onPairListDevices(data, callback) {
-		this.getSensorsByCondition(device => device.modelid === 'HM-HS1WL-M', callback)
+		this.getSensorsByCondition(device => device.modelid === 'HM-HS1WL-M' || device.modelid === 'FB56-WTS04HM1.1' || device.modelid === 'SWHM-I1' || device.modelid==='WaterSensor-EM', callback)
 	}
 	
 }
